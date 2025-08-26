a215644422809fce0fb26f7c291ee99311ea3094
Darshana Wishwajith <darshanawishwajith494@gmail.com>
2025-08-27 00:26:31 +0530

    finalized

--- src/components/BGCircle.jsx
+++ src/components/BGCircle.jsx
@@ -1,7 +1,7 @@
 const BGCircle = ({side}) => {
     return(
         <>
-            {side == 'left' ? <div className='w-150 h-150 bg-[#4071C5]/25 rounded-full blur-[100px] absolute left-0 -z-100'></div> : <div className='w-150 h-150 bg-[#4071C5]/25 rounded-full blur-[100px] absolute right-0 -z-100'></div>}
+            {side == 'left' ? <div className='w-150 h-150 bg-[#4071C5]/10 rounded-full blur-[100px] absolute left-0 -z-100'></div> : <div className='w-150 h-150 bg-[#4071C5]/10 rounded-full blur-[100px] absolute right-0 -z-100'></div>}
         </>
     )
 }
